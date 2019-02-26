7d1bdd43ac346e028214c4290f63a8ef4432d8f1
KishoreKaushal <kshr4kshl@gmail.com>
2019-02-26 21:29:15 +0530

    CtrlQ for quit

--- kilo/kilo.cpp
+++ kilo/kilo.cpp
@@ -7,6 +7,11 @@
 #include <termios.h>
 #include <unistd.h>
 
+/*** defines ***/
+
+#define CTRL_KEY(k) ((k) & 0x1f)
+
+
 /*** data ***/
 
 struct termios orig_termios;
@@ -75,7 +80,7 @@ int main() {
             printf("%d ('%c')\r\n", c, c);
         }
 
-        if (c == 'q') break;
+        if (c == CTRL_KEY('q')) break;
     }
 
     return 0;
