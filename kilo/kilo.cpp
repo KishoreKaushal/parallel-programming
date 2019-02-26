64bfa12e78286183a2db45eb07abe9d440261eac
KishoreKaushal <kshr4kshl@gmail.com>
2019-02-26 23:26:53 +0530

    clears lines one at a time

--- kilo/kilo.cpp
+++ kilo/kilo.cpp
@@ -153,6 +153,7 @@ void editorDrawRows(struct abuf *ab) {
     for (y = 0; y < E.screenrows; y++) {
         abAppend(ab, "~", 1);
 
+        abAppend(ab, "\x1b[K", 3);
         if (y < E.screenrows - 1) {
             abAppend(ab, "\r\n", 2);
         }
@@ -164,7 +165,7 @@ void editorRefreshScreen() {
 
 
     abAppend(&ab, "\x1b[?25l", 6);
-    abAppend(&ab, "\x1b[2J", 4);
+    // abAppend(&ab, "\x1b[2J", 4);
     abAppend(&ab, "\x1b[H", 3);
 
     editorDrawRows(&ab);
