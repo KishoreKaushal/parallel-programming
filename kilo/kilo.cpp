61627a358698842b3909107609e517fe6c521f35
KishoreKaushal <kshr4kshl@gmail.com>
2019-03-10 20:58:44 +0530

    horizontal scrolling

--- kilo/kilo.cpp
+++ kilo/kilo.cpp
@@ -50,6 +50,7 @@ struct erow {
 struct editorConfig {
     int cx, cy;
     int rowoff;
+    int coloff;
     int screenrows;
     int screencols;
     int numrows;
@@ -261,6 +262,13 @@ void editorScroll() {
     if (E.cy >= E.rowoff + E.screenrows) {
         E.rowoff = E.cy - E.screenrows + 1;
     }
+
+    if (E.cx < E.coloff){
+        E.coloff = E.cx;
+    }
+    if (E.cx >= E.coloff + E.screencols){
+        E.coloff = E.cx - E.screencols + 1;
+    }
 }
 
 void editorDrawRows(struct abuf *ab) {
@@ -300,10 +308,10 @@ void editorDrawRows(struct abuf *ab) {
                 abAppend(ab, "~", 1);
             }
         } else {
-            int len = E.row[filerow].size;
-            if (len > E.screencols)
-                len = E.screencols;
-            abAppend(ab, E.row[filerow].chars, len);
+            int len = E.row[filerow].size - E.coloff;
+            if (len < 0) len = 0;
+            if (len > E.screencols) len = E.screencols;
+            abAppend(ab, &E.row[filerow].chars[E.coloff], len);
         }
         
         abAppend(ab, "\x1b[K", 3);
@@ -325,7 +333,8 @@ void editorRefreshScreen() {
     editorDrawRows(&ab);
 
     char buf[32];
-    snprintf(buf, sizeof(buf), "\x1b[%d;%dH", (E.cy - E.rowoff) + 1, E.cx + 1);
+    snprintf(buf, sizeof(buf), "\x1b[%d;%dH", (E.cy - E.rowoff) + 1, (E.cx - E.coloff) + 1);
+    
     abAppend(&ab, buf, strlen(buf));
 
     abAppend(&ab, "\x1b[?25h", 6);
@@ -342,7 +351,7 @@ void editorMoveCursor(int key) {
             if (E.cx != 0) { E.cx--; }
             break;
         case ARROW_RIGHT:
-            if (E.cx != E.screencols - 1) { E.cx++; }
+            E.cx++;
             break;
         case ARROW_UP:
             if (E.cy != 0) { E.cy--; }
@@ -398,6 +407,7 @@ void initEditor() {
     E.cx = 0;
     E.cy = 0;
     E.rowoff = 0;
+    E.coloff = 0;
     E.numrows = 0;
     E.row = NULL;
 
