fb5599ee9033623de3db4b3ddaddbc1d679ef910
KishoreKaushal <kshr4kshl@gmail.com>
2019-03-11 13:56:00 +0530

    move to the end of the line with End

--- kilo/kilo.cpp
+++ kilo/kilo.cpp
@@ -447,7 +447,7 @@ void editorProcessKeypress() {
             break;
       
         case END_KEY:
-            E.cx = E.screencols - 1;
+            if (E.cy < E.numrows) E.cx = E.row[E.cy].size;
             break;
         
         case PAGE_UP:
@@ -459,7 +459,7 @@ void editorProcessKeypress() {
                 if (E.cy > E.numrows)
                     E.cy = E.numrows;
             }
-            
+
             int times = E.screenrows;
             while (times--){
                 editorMoveCursor(c == PAGE_UP ? ARROW_UP : ARROW_DOWN);
