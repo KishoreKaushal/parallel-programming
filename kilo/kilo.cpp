c19280c2f8dff826f79d648bc2be7dad438794a6
KishoreKaushal <kshr4kshl@gmail.com>
2019-03-11 13:24:58 +0530

    snap cursor to end of line

--- kilo/kilo.cpp
+++ kilo/kilo.cpp
@@ -364,6 +364,12 @@ void editorMoveCursor(int key) {
             if (E.cy != E.numrows) { E.cy++; }
             break;
     }
+
+    row = (E.cy >= E.numrows) ? NULL : &E.row[E.cy];
+    int rowlen = row ? row->size : 0;
+    if (E.cx > rowlen) {
+        E.cx = rowlen;
+    }
 }
 
 void editorProcessKeypress() {
