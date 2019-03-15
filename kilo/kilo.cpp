26eb9cb10d4788bf6308bfca165e51422fda2859
KishoreKaushal <kshr4kshl@gmail.com>
2019-03-15 18:27:43 +0530

    Prevent inserting special characters

--- kilo/kilo.cpp
+++ kilo/kilo.cpp
@@ -32,6 +32,7 @@
 #define CTRL_KEY(k) ((k) & 0x1f)
 
 enum editorKey {
+    BACKSPACE = 127,
     ARROW_LEFT = 1000,
     ARROW_RIGHT,
     ARROW_UP,
@@ -498,6 +499,10 @@ void editorProcessKeypress() {
     int c = editorReadKey();
 
     switch (c) {
+        case '\r':
+            /* TODO */
+            break;
+
         case CTRL_KEY('q'):
             write(STDOUT_FILENO, "\x1b[2J", 4);
             write(STDOUT_FILENO, "\x1b[H", 3);
@@ -512,6 +517,12 @@ void editorProcessKeypress() {
             if (E.cy < E.numrows) E.cx = E.row[E.cy].size;
             break;
         
+        case BACKSPACE:
+        case CTRL_KEY('h'):
+        case DEL_KEY:
+            /* TODO */
+            break;
+
         case PAGE_UP:
         case PAGE_DOWN: {
             if (c == PAGE_UP) {
@@ -535,7 +546,11 @@ void editorProcessKeypress() {
         case ARROW_RIGHT:
             editorMoveCursor(c);
             break;
-    
+
+        case CTRL_KEY('l'):
+        case '\x1b':
+            break;
+
         default:
             editorInsertChar(c);
             break;
