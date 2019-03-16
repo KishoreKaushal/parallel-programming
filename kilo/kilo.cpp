1df32214e76010ef02eb176037f31893f42fda35
KishoreKaushal <kshr4kshl@gmail.com>
2019-03-16 17:47:36 +0530

    restore syntax highlighting after search

--- kilo/kilo.cpp
+++ kilo/kilo.cpp
@@ -472,28 +472,43 @@ void editorSave() {
 
 /*** find ***/
 
-void editorFindCallback(char *query, int key) {
+void editorFindCallback(char *query, int key)
+{
     static int last_match = -1;
     static int direction = 1;
-    if (key == '\r' || key == '\x1b') {
+    static int saved_hl_line;
+    static char *saved_hl = NULL;
+    if (saved_hl)
+    {
+        memcpy(E.row[saved_hl_line].hl, saved_hl, E.row[saved_hl_line].rsize);
+        free(saved_hl);
+        saved_hl = NULL;
+    }
+    if (key == '\r' || key == '\x1b')
+    {
         last_match = -1;
         direction = 1;
         return;
-    } else if (key == ARROW_RIGHT || key == ARROW_DOWN) {
+    }
+    else if (key == ARROW_RIGHT || key == ARROW_DOWN)
+    {
         direction = 1;
-    } else if (key == ARROW_LEFT || key == ARROW_UP) {
+    }
+    else if (key == ARROW_LEFT || key == ARROW_UP)
+    {
         direction = -1;
-    } else {
+    }
+    else
+    {
         last_match = -1;
         direction = 1;
     }
-
     if (last_match == -1)
         direction = 1;
     int current = last_match;
     int i;
-    
-    for (i = 0; i < E.numrows; i++) {
+    for (i = 0; i < E.numrows; i++)
+    {
         current += direction;
         if (current == -1)
             current = E.numrows - 1;
@@ -501,12 +516,15 @@ void editorFindCallback(char *query, int key) {
             current = 0;
         erow *row = &E.row[current];
         char *match = strstr(row->render, query);
-        if (match) {
+        if (match)
+        {
             last_match = current;
             E.cy = current;
             E.cx = editorRowRxToCx(row, match - row->render);
             E.rowoff = E.numrows;
-
+            saved_hl_line = current;
+            saved_hl = (char*)malloc(row->rsize);
+            memcpy(saved_hl, row->hl, row->rsize);
             memset(&row->hl[match - row->render], HL_MATCH, strlen(query));
             break;
         }
